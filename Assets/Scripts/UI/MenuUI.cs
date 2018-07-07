f71e95aa0da66bb62925b60e6db92ded614f748b
Vlad Kabalov <vladskryto@gmail.com>
2018-07-07 16:52:22 +0400

    Remove rect transforming in script and add grid layout group to buttons

--- Assets/Scripts/UI/MenuUI.cs
+++ Assets/Scripts/UI/MenuUI.cs
@@ -3,17 +3,12 @@
 public class MenuUI : MonoBehaviour {
 
     GameObject pausePanel, resumeButton;
-    [SerializeField]
-    RectTransform restartButtonRect;
-    Vector2 buttonPosCenter = new Vector2(0.5f, 0.5f);
-    Vector2 buttonPosBottom = new Vector2(0.5f, 0.35f); 
 
     void Start()
     {
         SubscribeEvents();
         pausePanel = GameObject.Find("PausePanel");
         resumeButton = GameObject.Find("Resume");
-        restartButtonRect = GameObject.Find("Restart").GetComponent<RectTransform>();
         pausePanel.SetActive(false);
     }
 
@@ -21,28 +16,12 @@ public class MenuUI : MonoBehaviour {
     {
         pausePanel.SetActive(true);
         resumeButton.SetActive(false);
-        SetButtonRectOnRestart();
-    }
-
-    private void SetButtonRectOnPause()
-    {
-        restartButtonRect.anchorMin = buttonPosBottom;
-        restartButtonRect.anchorMax = buttonPosBottom;
-        restartButtonRect.anchoredPosition = Vector2.zero;
-    }
-
-    private void SetButtonRectOnRestart()
-    {
-        restartButtonRect.anchorMin = buttonPosCenter;
-        restartButtonRect.anchorMax = buttonPosCenter;
-        restartButtonRect.anchoredPosition = Vector2.zero;
     }
 
     private void Pause()
     {
         pausePanel.SetActive(true);
         resumeButton.SetActive(true);
-        SetButtonRectOnPause();
     }
 
     private void Resume()
